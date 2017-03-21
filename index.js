380a65da9546294f60f15fe304013e9877ec69fe
davemackintosh <me@davemackintosh.co.uk>
2017-03-21 18:49:08 +0000

    This is much nicer

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict"
+
+require("./warn")
+
+module.exports = require("multicolour")
---
+++ warn.js
@@ -0,0 +1,9 @@
+"use strict"
+
+console.info("---")
+console.info("---")
+console.info("This 'multicolor' package is a proxy package.")
+console.info("You should install multicolour (with a u) to remove this message.")
+console.info("This package will work as it is but it encouraged you install the official multicolour package.")
+console.info("---")
+console.info("---")
